092a988731db712485ba3d48e047b9034606a377
Arjun <85511086+arjun-com@users.noreply.github.com>
2023-12-14 20:20:52 +0530

    updated setup files to reflect new db columns.

--- setup/setup.sql
+++ setup/setup.sql
@@ -1,7 +1,10 @@
+create user "school-admin"@"localhost" identified by "password";
+grant all privileges on schoolProjectDB.* to "school-admin"@"localhost";
+
 create database schoolProjectDB;
 
 create table users(
-    uuid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT;
+    uuid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username varchar(100) NOT NULL,
     password varchar(64) NOT NULL,
     school varchar(100) NOT NULL,
@@ -9,11 +12,12 @@ create table users(
     section varchar(10),
     dob varchar(20),
     email varchar(100) NOT NULL,
-    role varchar(50) NOT NULL
+    role varchar(50) NOT NULL,
+	teaching_classes varchar(100) NOT NULL
 );
 
 create table assignments(
-    uaid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT;
+    uaid int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     subject varchar(50),
     startdate varchar(50),
     enddate varchar(50),
